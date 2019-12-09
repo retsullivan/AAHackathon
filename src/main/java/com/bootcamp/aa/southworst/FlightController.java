d88a5a25d5ba7470fe485da97978edf073efc129
Ethan Miller <Ethan.Miller@improving.com>
2019-12-09 10:40:02 -0600

    test flights client in flight controller

--- src/main/java/com/bootcamp/aa/southworst/FlightController.java
+++ src/main/java/com/bootcamp/aa/southworst/FlightController.java
@@ -1,12 +1,24 @@
 package com.bootcamp.aa.southworst;
 
+import com.bootcamp.aa.southworst.client.FlightsClient;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FlightController {
 
+    FlightsClient flightsClient;
 
-//        @GetMapping(path="/all")
+    public FlightController(FlightsClient flightsClient) {
+        this.flightsClient = flightsClient;
+    }
+
+    @GetMapping("/")
+    public String getAllFlights() {
+        return flightsClient.getFlights("2020-12-10");
+    }
+
+    //        @GetMapping(path="/all")
 //        public @ResponseBody Iterable<Flight> getAllFlights() {
 //            return ***Repository.findAll();
 //        }
