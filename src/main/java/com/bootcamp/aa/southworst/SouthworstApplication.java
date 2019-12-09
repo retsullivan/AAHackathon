894aa91a2afb8faa426aec37dd37c5714533958f
Zach Cannon <zach.cannon@improving.com>
2019-12-09 09:44:54 -0600

    Creating application

---
+++ src/main/java/com/bootcamp/aa/southworst/SouthworstApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.aa.southworst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthworstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthworstApplication.class, args);
+	}
+
+}
