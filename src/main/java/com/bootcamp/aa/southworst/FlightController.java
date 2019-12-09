e1e6aadf2cf72c9d1dd3664274e904d302798b04
Paloma Pineda <Paloma.Pineda@improving.com>
2019-12-09 10:17:24 -0600

    Added Spring web dependency

---
+++ src/main/java/com/bootcamp/aa/southworst/FlightController.java
@@ -0,0 +1,13 @@
+package com.bootcamp.aa.southworst;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FlightController {
+
+
+//        @GetMapping(path="/all")
+//        public @ResponseBody Iterable<Flight> getAllFlights() {
+//            return ***Repository.findAll();
+//        }
+}
