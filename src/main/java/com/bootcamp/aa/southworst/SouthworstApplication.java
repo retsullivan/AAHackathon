d2f1b8fbce514836fc70d2835bd7ce5838439e77
Zach Cannon <zach.cannon@improving.com>
2019-12-09 14:48:04 -0600

    Fixing cors

--- src/main/java/com/bootcamp/aa/southworst/SouthworstApplication.java
+++ src/main/java/com/bootcamp/aa/southworst/SouthworstApplication.java
@@ -2,12 +2,19 @@ package com.bootcamp.aa.southworst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SouthworstApplication {
+public class SouthworstApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SouthworstApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SouthworstApplication.class, args);
+    }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
 }
